389ca547baa0eac46ce28175aa2e553303b77a94
labdav <93351441+labdav@users.noreply.github.com>
2022-02-08 09:29:48 -0500

    premier push dans dev

--- public/wp-content/themes/timcsf_2022/functions.php
+++ public/wp-content/themes/timcsf_2022/functions.php
@@ -26,3 +26,45 @@ if (function_exists('register_nav_menus')) {
 }
 
 ?>
+
+<?php
+function tim_responsable_custom_post() {
+
+    $labels = array(
+
+        'name'                => _x( 'Responsables de la TIM', 'Post Type General Name'),
+
+        'singular_name'       => _x( 'Responsables', 'Post Type Singular Name'),
+
+        'menu_name'           => __( 'Responsables 2021'),
+
+        'all_items'           => __( 'Tous nos responsables'),
+        'view_item'           => __( 'Voir nos responsables'),
+        'add_new_item'        => __( 'Ajouter un nouveau responsable'),
+        'add_new'             => __( 'Ajouter'),
+        'edit_item'           => __( 'Editer un responsable'),
+        'update_item'         => __( 'Modifier un responsable'),
+        'search_items'        => __( 'Rechercher un responsable'),
+        'not_found'           => __( 'Non trouvé'),
+        'not_found_in_trash'  => __( 'Non trouvé dans la corbeille'),
+    );
+
+
+    $args = array(
+        'label'               => __( 'Nos responsables'),
+        'description'         => __( 'Tous sur nos responsables'),
+        'labels'              => $labels,
+        'supports'            => array( 'title', 'thumbnail'),
+        'hierarchical'        => false,
+        'public'              => true,
+        'has_archive'         => true,
+        'rewrite'			  => array( 'slug' => 'responsables'),
+    );
+
+
+    register_post_type( 'responsables', $args );
+}
+
+add_action( 'init', 'tim_responsable_custom_post', 0 );
+
+?>
